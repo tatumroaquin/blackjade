5207e2cf6e25fc5ada7c2138b75ba3103cf79fa8
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-02-09 20:59:52 +1100

    calculate mining rate script

---
+++ backend/average-work.js
@@ -0,0 +1,26 @@
+import Blockchain from './src/blockchain/chain.js';
+
+const blockchain = new Blockchain();
+const timeDiffs = [];
+
+blockchain.addBlock({ data: 'initiate' });
+
+for (let i = 1; i < 500; i++) {
+  let prevBlock = blockchain.chain.slice(-1).pop();
+
+  blockchain.addBlock({ data: `block ${i}` });
+
+  let nextBlock = blockchain.chain.slice(-1).pop();
+
+  let timeDiff = nextBlock.timestamp - prevBlock.timestamp;
+  timeDiffs.push(timeDiff);
+
+  let average =
+    timeDiffs.reduce((total, time) => total + time) / timeDiffs.length;
+
+  console.log(
+    `ETA ${timeDiff}ms. Difficulty ${
+      prevBlock.difficulty
+    }. AverageWork ${average.toFixed(3)}ms`
+  );
+}
