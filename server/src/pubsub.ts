26969898b274adf99c8ea60cc5980f6b98edfa58
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-02-20 13:48:26 +1100

    * [feat] PubSub class to interact with the PubNub API.   - any peer can publish a message containing a blockchain JSON string.   - ignores any message coming from the node that sent that JSON string.

--- server/src/pubsub.ts
+++ server/src/pubsub.ts
@@ -1,31 +1,63 @@
 import PubNub from 'pubnub';
-import { PUBNUB_KEYS } from './config.js';
+import Blockchain from './blockchain/chain.js';
+import { PUBNUB_KEYS, NODE_ID } from './config.js';
+
+type Message = {
+  channel: string;
+  message: string;
+  publisher?: string;
+};
 
 const CHANNELS: { [CHANNEL: string]: string } = {
   TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
 };
 
 export default class PubSub {
+  blockchain: Blockchain;
   pubnub: PubNub;
 
-  constructor() {
+  constructor({ blockchain }: { blockchain: Blockchain }) {
+    this.blockchain = blockchain;
     this.pubnub = new PubNub(PUBNUB_KEYS);
-
     this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
-
     this.pubnub.addListener(this.listener());
   }
 
   listener() {
     return {
-      message: (messageObject: { channel: string; message: string }): void => {
-        const { channel, message } = messageObject;
-        console.log(`CHANNEL: ${channel}, MESSAGE: ${message}`);
+      message: (messageObject: Message): void => {
+        const { channel, message, publisher } = messageObject;
+        console.log(`CHANNEL: ${channel}\nNODE: ${publisher}\nMESSAGE: ${message}
+        `);
+        this.handleMessage({ channel, message, publisher });
       },
     };
   }
 
-  publish({ channel, message }: { channel: string; message: JSON | string }) {
-    this.pubnub.publish({ channel, message });
+  handleMessage({ channel, message, publisher }: Message) {
+    // message came from myself? ignore it
+    if (publisher === NODE_ID) return;
+
+    const parsedData = JSON.parse(message);
+
+    switch (channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedData);
+        break;
+    }
+  }
+
+  publish({ channel, message }: Message) {
+    (async () => {
+      await this.pubnub.publish({ channel, message });
+    })();
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain),
+    });
   }
 }
