b734b1f9fb77652ea1b67d66d4b503827b9fdb5b
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-02-20 13:46:22 +1100

    * [feat] publisher/subscriber module to broadcast blockchains P2P. * [feat] local peer will spawn a random port on the same address.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import Blockchain from './blockchain/chain.js';
-
-dotenv.config()
+import PubSub from './pubsub.js';
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+setTimeout(() => pubsub.broadcastChain(), 1000)
 
 app.get('/api/blocks', (_: Request, res: Response) => {
   res.json(blockchain.chain);
@@ -17,10 +18,19 @@ app.get('/api/blocks', (_: Request, res: Response) => {
 app.post('/api/mine', (req: Request, res: Response) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+  pubsub.broadcastChain();
   res.redirect('/api/blocks');
 });
 
-const PORT = process.env.PORT || 3000;
+const ROOT_PORT = process.env.PORT ?? 3000;
+let PEER_PORT: number | undefined;
+
+if (Boolean(process.env.LOCAL_PEER) === true) {
+  PEER_PORT = Number(ROOT_PORT) + Math.floor(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT ?? ROOT_PORT;
+
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
