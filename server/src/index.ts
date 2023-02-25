58237e0d7fdae643946384fd9f233847e1cab74c
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-02-26 08:17:40 +1100

    [feat]: Sync blockchains - Spawning a new node will trigger a sync chains event to sync up with   the root node. - When the retrieved root blockchain contains a longer list, the current node triggers a reclace chain event. - Refactor and clean up port number generation logic.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from 'express';
+import fetch from 'node-fetch';
+import Block from './blockchain/block.js';
 import Blockchain from './blockchain/chain.js';
 import PubSub from './pubsub.js';
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+const { ROOT_NODE_ADDRESS } = process.env;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-setTimeout(() => pubsub.broadcastChain(), 1000)
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.get('/api/blocks', (_: Request, res: Response) => {
   res.json(blockchain.chain);
@@ -22,15 +25,29 @@ app.post('/api/mine', (req: Request, res: Response) => {
   res.redirect('/api/blocks');
 });
 
-const ROOT_PORT = process.env.PORT ?? 3000;
-let PEER_PORT: number | undefined;
-
-if (Boolean(process.env.LOCAL_PEER) === true) {
-  PEER_PORT = Number(ROOT_PORT) + Math.floor(Math.random() * 1000);
+async function syncBlockchains() {
+  const response = await fetch(`${ROOT_NODE_ADDRESS}/api/blocks`);
+  if (!response.ok || response.type === 'error') {
+    console.log('error fetching root node blockchain');
+    return;
+  }
+  console.log(`sync chains event, fetching from root "${ROOT_NODE_ADDRESS}"`);
+  const rootChain = (await response.json()) as Block[];
+  blockchain.replaceChain(rootChain);
 }
 
-const PORT = PEER_PORT ?? ROOT_PORT;
+function getPortNumber() {
+  const LOCAL_PEER = Boolean(process.env.LOCAL_PEER);
+  const ROOT_PORT = process.env.PORT ?? 3000;
+  if (!LOCAL_PEER) return ROOT_PORT;
 
+  return Number(ROOT_PORT) + Math.floor(Math.random() * 1000);
+}
+
+const PORT = getPortNumber();
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
+  (async () => {
+    await syncBlockchains();
+  })();
 });
