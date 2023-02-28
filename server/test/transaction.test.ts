a7af28e3cc9497c850dafd618a217ee8516180a5
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-02-28 18:34:57 +1100

    test(transaction): Add checks for Transaction objects
    - check tx input and output correctness
    - check transaction validation logic

---
+++ server/test/transaction.test.ts
@@ -0,0 +1,101 @@
+import Wallet from '../src/wallet/wallet.js';
+import Transaction from '../src/transaction/transaction.js';
+import { Input, Output } from '../src/transaction/transaction.d.js';
+
+describe('Transaction', () => {
+  let transaction: Transaction;
+  let senderWallet: Wallet;
+  let receiverWallet: Wallet;
+  beforeEach(() => {
+    senderWallet = new Wallet();
+    receiverWallet = new Wallet();
+    transaction = new Transaction({ senderWallet, receiverWallet, amount: 1 });
+  });
+  describe('check tx properties', () => {
+    it('has a unique id', () => {
+      expect(transaction.id).toBeDefined();
+    });
+    it('has a an tx input object', () => {
+      expect(transaction.input).toBeDefined();
+    });
+    it('has a an tx input object', () => {
+      expect(transaction.output).toBeDefined();
+    });
+  });
+
+  describe('createOutput()', () => {
+    it('returns a valid tx output object', () => {
+      const amount = 1;
+      const mockOutput = {
+        [receiverWallet.keypair.getPublicKey()]: amount,
+        [senderWallet.keypair.getPublicKey()]: senderWallet.balance - amount,
+      };
+      expect(
+        transaction.createOutput({ senderWallet, receiverWallet, amount })
+      ).toEqual(mockOutput);
+    });
+  });
+
+  describe('createInput()', () => {
+    let amount: number;
+    let output: Output;
+    let input: Input;
+
+    beforeEach(() => {
+      amount = 1;
+      output = transaction.createOutput({
+        senderWallet,
+        receiverWallet,
+        amount,
+      });
+      input = transaction.createInput({ senderWallet, output });
+    });
+
+    describe('check tx input properties', () => {
+      it('has a numerical timestamp', () => {
+        expect(input.timestamp).toBeDefined();
+        expect(typeof input.timestamp).toBe('number');
+      });
+
+      it('has a sender wallet address', () => {
+        expect(input.wallet).toBeDefined();
+        expect(typeof input.wallet).toBe('string');
+      });
+
+      it('has a record of input balance', () => {
+        expect(input.amount).toBeDefined();
+        expect(typeof input.amount).toBe('number');
+      });
+
+      it('has a signature for the output', () => {
+        expect(input.signature).toBeDefined();
+        expect(typeof input.signature).toBe('string');
+      });
+    });
+  });
+
+  describe('isValidTransaction()', () => {
+    describe('transaction has invalid total output amount', () => {
+      it('returns false', () => {
+        Object.keys(transaction.output).map(
+          (wallet) =>
+            (transaction.output[wallet] = transaction.output[wallet] + 1)
+        );
+        expect(Transaction.isValidTransaction(transaction)).toBe(false);
+      });
+    });
+    describe('transaction has invalid signature', () => {
+      it('returns false', () => {
+        let evilSignature = transaction.input.signature.replace(/[0-9]/g, '1');
+        evilSignature = evilSignature.replace(/[a-f]/g, 'c');
+        transaction.input.signature = evilSignature;
+        expect(Transaction.isValidTransaction(transaction)).toBe(false);
+      });
+    });
+    describe('transaction is completely valid', () => {
+      it('returns true', () => {
+        expect(Transaction.isValidTransaction(transaction)).toBe(true);
+      });
+    });
+  });
+});
