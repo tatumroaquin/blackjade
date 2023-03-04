e7ada5abc6dee92d0085e0d0e6a5ef08dfb98829
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-04 14:27:10 +1100

    test(wallet): Add checks for balance calculation.
    - check calculateBalance() returns a balance from a wallet and blockchain.
    - check balance when NO existing transactions is found in the blockchain.
    - check balance is added when wallet receives funds, without sending funds.
    - check balance is subtracted, when wallet is sending funds.

--- server/test/wallet.test.ts
+++ server/test/wallet.test.ts
@@ -1,6 +1,7 @@
 import Wallet from '../src/wallet/wallet.js';
 import KeyPair from '../src/wallet/keypair.js';
 import Transaction from '../src/transaction/transaction.js';
+import Blockchain from '../src/blockchain/chain.js';
 import { INITIAL_BALANCE } from '../src/config.js';
 
 describe('Wallet', () => {
@@ -56,6 +57,78 @@ describe('Wallet', () => {
         expect(transaction).toBe(undefined);
         expect(consoleErrorSpy).toBeCalled();
         consoleErrorSpy.mockRestore();
+      });
+    });
+  });
+
+  describe('calculateBalance()', () => {
+    let blockchain: Blockchain;
+
+    beforeEach(() => {
+      blockchain = new Blockchain();
+    });
+
+    describe('`Wallet` does NOT have outputs in the blockchain', () => {
+      it('returns the `INITIAL_BALANCE`', () => {
+        const balance = Wallet.calculateBalance({
+          chain: blockchain.chain,
+          address: wallet.getPublicKey(),
+        });
+        expect(balance).toEqual(INITIAL_BALANCE);
+      });
+    });
+
+    describe('`Wallet` IS receiving funds from the blockchain', () => {
+      let transaction1: Transaction | undefined;
+      let transaction2: Transaction | undefined;
+
+      beforeEach(() => {
+        transaction1 = new Wallet().createTransaction({
+          recipientAddress: wallet.publicKey,
+          amount: 2,
+        });
+        transaction2 = new Wallet().createTransaction({
+          recipientAddress: wallet.publicKey,
+          amount: 3,
+        });
+        blockchain.addBlock({ data: [transaction1, transaction2] });
+      });
+
+      it('adds the sum from `INITIAL_BALANCE` + t1 + t2 ...', () => {
+        const balance = Wallet.calculateBalance({
+          chain: blockchain.chain,
+          address: wallet.publicKey,
+        });
+        expect(balance).toEqual(
+          INITIAL_BALANCE +
+            transaction1!.output[wallet.getPublicKey()] +
+            transaction2!.output[wallet.getPublicKey()]
+        );
+      });
+    });
+
+    describe('`Wallet` IS sending funds to the blockchain', () => {
+      let transaction: Transaction | undefined;
+
+      beforeEach(() => {
+        transaction = wallet.createTransaction({
+          recipientAddress: new Wallet().getPublicKey(),
+          amount: 2,
+        });
+        transaction!.update({
+          senderWallet: wallet,
+          recipientAddress: new Wallet().getPublicKey(),
+          amount: 3,
+        });
+        blockchain.addBlock({ data: [transaction] });
+      });
+
+      it('subtracts the sum from `INITIAL_BALANCE` - tx', () => {
+        const balance = Wallet.calculateBalance({
+          chain: blockchain.chain,
+          address: wallet.getPublicKey(),
+        });
+        expect(balance).toEqual(transaction!.output[wallet.getPublicKey()]);
       });
     });
   });
