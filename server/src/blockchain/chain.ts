b9bf096dbfc3c8c2372033748bf76d2e84f9efc4
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-09 14:37:07 +1100

    feat(blockchain): Add validation for existing blockchain transaction objects.
    - isValidTransactions() test whether each tx object is valid.
    - ensure the first block matches the genesis block
    - ensure no duplicate reward tx is present in each block.
    - ensure miner reward value matches the `MINING_REWARD`
    - ensure each transaction object is cryptographically valid.
    - ensure that tx objects are unique, prevent double-spending.
    - ensure wallet's balance matches with the existing blockchain.

--- server/src/blockchain/chain.ts
+++ server/src/blockchain/chain.ts
@@ -1,4 +1,7 @@
+import { MINING_REWARD, GENESIS_BLOCK } from '../config.js';
 import Block from './block.js';
+import Transaction from '../transaction/transaction.js';
+import Wallet from '../wallet/wallet.js';
 import sha256 from '../utility/sha256.js';
 
 export default class Blockchain {
@@ -14,7 +17,7 @@ export default class Blockchain {
     this.chain.push(newBlock);
   }
 
-  static isValidChain(chain: Block[]) {
+  static isValidChain(chain: Block[]): boolean {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.getGenesis()))
       return false;
 
@@ -49,5 +52,64 @@ export default class Blockchain {
     }
 
     this.chain = chain;
+  }
+
+  isValidTransactions(chain: Block[]): boolean {
+    const genesisBlock = JSON.stringify(Block.getGenesis());
+    const firstBlock = JSON.stringify(chain[0]);
+
+    if (firstBlock !== genesisBlock) return false;
+
+    for (let block of chain) {
+      let minerRewardCount = 0;
+      let transactionSet = new Set();
+
+      for (let transaction of block.data) {
+        if (transaction.id === GENESIS_BLOCK.data[0].id) break;
+
+        if (transaction.input?.type === 'MINER-REWARD') {
+          minerRewardCount++;
+
+          // only one miner reward tx object must exist
+          if (minerRewardCount > 1) {
+            console.error('Duplicate reward transactions found!');
+            return false;
+          }
+
+          // check miner reward value matches MINING_REWARD constant
+          if (Object.values(transaction.output)[0] !== MINING_REWARD) {
+            console.error('Miner reward value is invalid!');
+            return false;
+          }
+          continue;
+        }
+
+        if (!Transaction.isValidTransaction(transaction)) {
+          console.error('Invalid transaction found!');
+          return false;
+        }
+
+        // Patch double-spending problem
+        if (transactionSet.has(transaction)) {
+          console.error('Duplicate transactions found!');
+          return false;
+        }
+
+        // we must supply `this.chain` to check the real balance
+        // this is also the reason why this method is not static
+        const existingBalance = Wallet.calculateBalance({
+          chain: this.chain,
+          address: transaction.input.wallet,
+        });
+
+        if (transaction.input.amount !== existingBalance) {
+          console.error('Invalid transaction input amount!');
+          return false;
+        }
+
+        transactionSet.add(transaction);
+      }
+    }
+    return true;
   }
 }
