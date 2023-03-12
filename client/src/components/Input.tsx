e29d157432af197c571ca06319d0f40f78957d44
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-12 20:15:16 +1100

    feat(input): Stylistic changes to input component

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,6 +1,9 @@
+import styles from './Input.module.scss';
+
 interface Input<T> {
   id: T;
   name: T;
+  label: T;
   placeholder: T;
   type: T;
   value?: T;
@@ -8,18 +11,25 @@ interface Input<T> {
 
 export const Input = ({
   id,
+  label,
   name,
   placeholder,
   type,
   value,
 }: Input<string>) => {
   return (
-    <input
-      id={`${id}`}
-      name={`${name}`}
-      placeholder={`${placeholder}`}
-      type={`${type}`}
-      value={`${value}`}
-    />
+    <div className={styles['input__container']}>
+      <label className={styles['input__label']} htmlFor={`${id}`}>
+        {label}
+      </label>
+      <input
+        className={styles['input__field']}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+      />
+    </div>
   );
 };
