135ea8c56352771939d5b17352cdc745ea7c6d80
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-12 20:17:20 +1100

    feat: Add a custom styled boilerplate login form

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,30 @@
+import { Input } from './Input';
+import { Button } from './Button';
+import styles from './LoginForm.module.scss';
+
+export const LoginForm = () => {
+  return (
+    <div className={styles['login__container']}>
+      <form className={styles['login__form']}>
+        <h1 className={styles['login__title']}>Login</h1>
+        <Input
+          id='username'
+          label='Username'
+          name='username'
+          type='text'
+          placeholder='Enter Username'
+        />
+        <Input
+          id='password'
+          label='Password'
+          name='password'
+          type='password'
+          placeholder='Enter Password'
+        />
+        <Button type='button' disabled={false}>
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+};
