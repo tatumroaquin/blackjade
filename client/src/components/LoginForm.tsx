2f0706ff3c56c1483d5f156d9a24eec26b79fd0a
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-13 23:15:30 +1100

    chore: Mock login form styling match colorscheme

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
-import { Input } from './Input';
-import { Button } from './Button';
+import { Input } from './UI/Input';
+import { Button } from './UI/Button';
 import styles from './LoginForm.module.scss';
 
 export const LoginForm = () => {
