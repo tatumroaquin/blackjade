3a4f93c4b09ff7409a58b27afd724bec1d472e5e
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-22 23:46:33 +1100

    feat(router): Add routing to the main entry point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,38 @@
-import { Login } from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
 import { Block } from './components/Block';
+import { BlockChain } from './components/BlockChain';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { NavBar } from './components/Nav/NavBar';
+import { Transactions } from './components/Transactions';
+
 import { DUMMY_BLOCKCHAIN } from './dummy/BLOCKCHAIN';
+
 import './App.scss';
 
 export default function App() {
-  const {chain} = DUMMY_BLOCKCHAIN;
-  const block = chain[2];
+  const { chain } = DUMMY_BLOCKCHAIN;
+  const block = chain[1];
+
+  const [showNavBar, setShowNavBar] = useState<boolean>(true);
+
   return (
     <>
-      <Block block={block} />
+      <Router>
+        {showNavBar && <NavBar />}
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path='/blockchain' element={<BlockChain chain={chain} />} />
+          <Route path='/block/:id' element={<Block block={block} />} />
+          <Route path='/tx-pool' element={<Transactions transactions={block.data}/>} />
+          <Route
+            path='/login'
+            element={<Login setShowNavBar={setShowNavBar} />}
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
