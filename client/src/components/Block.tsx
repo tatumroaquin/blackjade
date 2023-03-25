17d1028f665d163ed4f2ac2c2b78a4f174527743
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-25 22:29:11 +1100

    feat: Call `useHttp` hook in `Block` view.
    - use the custom http hook to retrieve details about individual blocks.
    - add state to store detalis about previous and next block hashes.
    - add rudimentary pagination to previous and next blocks in the chain.

--- client/src/components/Block.tsx
+++ client/src/components/Block.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { useHttp } from '../hooks/use-http';
+
 import { Link, useParams } from 'react-router-dom';
 import { Transactions } from './Transactions';
 import { Button } from './UI/Button';
@@ -9,70 +11,101 @@ import styles from './Block.module.scss';
 // export const Block: FC<{ block: _Block }> = ({ block }) => {
 export const Block: FC = () => {
   const { hashId } = useParams();
+  const [block, setBlock] = useState<_Block>();
+  const [prevBlockHash, setPrevBlockHash] = useState<string>('');
+  const [nextBlockHash, setNextBlockHash] = useState<string>('');
 
-  const block: _Block = DUMMY_BLOCKCHAIN.chain.find(
-    (block) => block.hash === hashId
-  )!;
+  const [blockIndex, setBlockIndex] = useState<number>(0);
 
-  const index: number = DUMMY_BLOCKCHAIN.chain.findIndex(
-    (block) => block.hash === hashId
-  );
+  const { sendRequest, isLoading, error, clearError } = useHttp();
 
-  let prevBlockHash: string;
-  let nextBlockHash: string;
+  useEffect(() => {
+    const abortController = new AbortController();
 
-  if (index > 0) prevBlockHash = DUMMY_BLOCKCHAIN.chain[index - 1].hash;
+    // GET CURRENT BLOCK BY HASH
+    (async () => {
+      try {
+        const data = await sendRequest({
+          url: `http://localhost:3000/api/block/hash/${hashId}`,
+          abortController,
+        });
+        setBlockIndex(data.index);
+        setBlock(data.block);
+      } catch (error) {}
+    })();
 
-  if (index < DUMMY_BLOCKCHAIN.chain.length - 1)
-    nextBlockHash = DUMMY_BLOCKCHAIN.chain[index + 1].hash;
+    // GET PREVIOUS BLOCK BY INDEX
+    (async () => {
+      try {
+        const data = await sendRequest({
+          url: `http://localhost:3000/api/block/index/${blockIndex - 1}`,
+          abortController,
+        });
+        setPrevBlockHash(data.hash);
+      } catch (error) {}
+    })();
 
-  const { timestamp, prevHash, hash, nonce, difficulty, data } = block;
+    // GET NEXT BLOCK BY INDEX
+    (async () => {
+      try {
+        const data = await sendRequest({
+          url: `http://localhost:3000/api/block/index/${blockIndex + 1}`,
+          abortController,
+        });
+        setNextBlockHash(data.hash);
+      } catch (error) {}
+    })();
+  }, [sendRequest]);
+
+  const { timestamp, prevHash, hash, nonce, difficulty, data } = block ?? {};
 
   return (
     <>
       <div className={styles['block__paging']}>
-        {prevBlockHash! && (
+        {prevBlockHash && (
           <Link to={`/block/${prevBlockHash}`}>
             <Button>&lt;</Button>
           </Link>
         )}
-        {nextBlockHash! && (
+        {nextBlockHash && (
           <Link to={`/block/${nextBlockHash}`}>
             <Button>&gt;</Button>
           </Link>
         )}
       </div>
-      <div className={styles['block']}>
-        <div className={styles['hash']}>
-          <label className={styles['hash__label']} htmlFor='hash'>
-            Hash
-          </label>
-          <h1 id='hash'>{hash}</h1>
-        </div>
+      {!isLoading && block && (
+        <div className={styles['block']}>
+          <div className={styles['hash']}>
+            <label className={styles['hash__label']} htmlFor='hash'>
+              Hash
+            </label>
+            <h1 id='hash'>{hash}</h1>
+          </div>
 
-        <table className={styles['block__info']}>
-          <thead>
-            <tr>
-              <th>Timestamp</th>
-              <th>Prev Hash</th>
-              <th>Nonce</th>
-              <th>Difficulty</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{new Date(timestamp).toUTCString()}</td>
-              <td>{prevHash}</td>
-              <td>{nonce}</td>
-              <td>{difficulty}</td>
-            </tr>
-          </tbody>
-        </table>
+          <table className={styles['block__info']}>
+            <thead>
+              <tr>
+                <th>Timestamp</th>
+                <th>Prev Hash</th>
+                <th>Nonce</th>
+                <th>Difficulty</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{new Date(timestamp).toUTCString()}</td>
+                <td>{prevHash}</td>
+                <td>{nonce}</td>
+                <td>{difficulty}</td>
+              </tr>
+            </tbody>
+          </table>
 
-        <div className={styles['block__data']}>
-          <Transactions transactions={data} />
+          <div className={styles['block__data']}>
+            <Transactions transactions={data} />
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
