0280abe56a52e817e6482b1bd289be42f482b271
Tatum Roaquin <19354579+tatumroaquin@users.noreply.github.com>
2023-03-25 22:32:26 +1100

    feat: Call `useHttp` to retrieve blockchain data.
    - add state to store blockchain data in the component.
    - use the custom http hook to GET the blockchain from backend.
    - spread blockchain in reverse order and display on frontend.

--- client/src/components/BlockChain.tsx
+++ client/src/components/BlockChain.tsx
@@ -1,6 +1,8 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useHttp } from '../hooks/use-http';
 import { Link } from 'react-router-dom';
 import { Card } from './UI/Card';
+import { _Block, _BlockChain } from '../types';
 import styles from './BlockChain.module.scss';
 
 interface Block {
@@ -17,34 +19,52 @@ interface BlockChain {
 }
 
 export const BlockChain: FC<BlockChain> = ({ chain }) => {
+  const [blockchain, setBlockchain] = useState<_BlockChain>();
+  const { sendRequest, isLoading, error, clearError } = useHttp();
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    (async () => {
+      try {
+        const data = await sendRequest({
+          url: 'http://localhost:3000/api/blockchain',
+          abortController,
+        });
+        setBlockchain(data);
+      } catch (error) {}
+    })();
+  }, [sendRequest]);
+
   return (
-  <div className={styles['container']}>
-    <Card>
-      <table className={styles['block-chain']}>
-        <thead>
-          <tr>
-            <th>Height</th>
-            <th>Hash</th>
-            <th>Timestamp</th>
-            <th>Nonce</th>
-            <th>Tx Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {[...chain].reverse().map((block, index) => (
-            <tr key={block.hash}>
-              <td>{chain.length - index - 1}</td>
-              <td>
-                <Link to={`/block/${block.hash}`}>{block.hash}</Link>
-              </td>
-              <td>{new Date(block.timestamp).toUTCString()}</td>
-              <td>{block.nonce}</td>
-              <td>{block.data.length}</td>
+    <div className={styles['container']}>
+      <Card>
+        <table className={styles['block-chain']}>
+          <thead>
+            <tr>
+              <th>Height</th>
+              <th>Hash</th>
+              <th>Timestamp</th>
+              <th>Nonce</th>
+              <th>Tx Count</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </Card>
+          </thead>
+          <tbody>
+            {blockchain &&
+              [...blockchain.chain].reverse().map((block, index) => (
+                <tr key={block.hash}>
+                  <td>{chain.length - index - 1}</td>
+                  <td>
+                    <Link to={`/block/${block.hash}`}>{block.hash}</Link>
+                  </td>
+                  <td>{new Date(block.timestamp).toUTCString()}</td>
+                  <td>{block.nonce}</td>
+                  <td>{block.data.length}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </Card>
     </div>
   );
 };
